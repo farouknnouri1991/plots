af465c3c5f9fb2d94031553d116dd78fb9e15368
farouknnouri1991 <46449157+farouknnouri1991@users.noreply.github.com>
2019-03-23 03:28:12 +0100

    Add files via upload

---
+++ appv1.R
@@ -0,0 +1,178 @@
+library(shiny); library(tidyverse); library(plotly)
+ #dta<-read.csv("dta.csv") %>% select(-1); L<-as.list(names(dta)); names(L)<-names(dta)
+#dta<-read.csv("Agriculture.csv") 
+#dta<-dta; L<-as.list(names(dta)); names(L)<-names(dta)
+#plottypes<-list(geom_path=geom_path,geom_point=geom_point)
+#in applying to GGDC: no need for large format: 
+#You can filter on indicator names and process(mutate, group by, etc) data within server.
+
+
+#Build UI----
+ui<-fluidPage(
+ 
+   
+  titlePanel("Lesson 4"),
+  sidebarLayout(position="left",
+    sidebarPanel(
+      
+      helpText("You will select a variable that will be dispalyed in the main panel"),
+      
+      #Select_DataBase
+      
+      selectInput(inputId = "selected_DB", label=p("Choose a Variable for Y axis"), choices= list("GGDC_PPP_large", "Agriculture")),
+      
+      #Select_variables
+
+      uiOutput("axe_x"), #( given the database==>reactive input==>comes from output and is Ui object: uiOutput)
+      uiOutput("axe_y"),
+      
+ 
+      
+      #plottype
+      
+        #which geom
+      
+      selectInput(inputId = "selected_geom", label=p("Choose a plot type", style="font-family:'times'") ,
+                  choices= names(list(geom_path=geom_path,geom_point=geom_point)) ), 
+      
+        #which color
+      
+      uiOutput("color"),
+      
+        #which size
+      
+      #filters:
+      
+        #Regions
+      
+      uiOutput(("regions")),
+      
+        #Countries
+      
+      uiOutput(("countries")),
+      
+      
+      #Button d'actualisation
+      
+      submitButton(text="Submit")
+      
+    
+    ),
+    
+    
+    mainPanel(
+      #textOutput("reactiveVars"),
+      
+      plotlyOutput("reactiveplot")
+      #dataTableOutput("dta")
+      #dataTableOutput("axes")
+    )
+  )
+  
+)
+
+    #server function mentions two arguments: output is a list-like object that stores instructions for building the R objects in your app.
+    #input is a second list-like object. It stores the current values of all of the widgets in your app. 
+    #These values will be saved under the names that you gave the widgets in your ui.here "var" and "range" as input$var and input$range
+    #slider widget has two values (a min and a max)=> input$range will contain a vector of length two
+
+#Build server----
+
+server<-function(input, output){
+  
+    #output$reactiveVars<-renderText(paste("Vous avez selectionne les variable: ", input$selected_var1, input$selected_var2))
+    
+    
+    #Reading dataset (to propose the axis in UI, otherwise I can make it in reactiveplot)----
+    
+        #reading data interactively and ist variables(axes, color, regions, and countries)
+        #Pour alimenter l'interface UI using uiOutput("")
+  
+            dta<-reactive({ read.csv(paste0(input$selected_DB,".csv")) })#reactive ++ to read it each time it changed (inout$selected_DB) and store it reactively
+            
+            output$dta<-renderDataTable({ 
+              
+              dta()
+             
+              })
+            
+            output$axe_x<-renderUI({
+             
+              selectInput(inputId = "selected_var1", label=p("Choose a Variable for X axis"), 
+                          choices= names(dta()), selected="Year")
+            })
+        
+            output$axe_y<-renderUI({
+              selectInput(inputId = "selected_var2", label=p("Choose Variables for Y axis"), 
+                          choices= names(dta()), multiple=T)
+            })
+            
+            output$color<-renderUI({
+              selectInput(inputId = "selected_color", label=p("Choose a color"), names(dta())[2:4] , selected=NULL)
+            })
+            
+            output$regions<-renderUI({
+               selectInput(inputId = "selected_region", label=p("Choose a region"), choices= as.list(unique(dta()[3])), multiple = T )
+            })
+            
+            output$countries<-renderUI({
+              selectInput(inputId = "selected_area", label=p("Choose a coutry"), choices= as.list(unique(dta()[2])), multiple=T )
+            })
+        
+    
+    
+     #Making the Plot----
+      output$reactiveplot<-renderPlotly ({
+      
+      #reading inputs
+        dta<-dta()
+        
+       
+        
+        x<-sym(input$selected_var1)#au lieu de switch c est mieux
+        y<-sym(input$selected_var2[1])
+        
+       
+        
+        if (length(input$selected_var2)<2){z<-y} else{z<-sym(input$selected_var2[2])}
+        col<-sym(input$selected_color)
+        
+        geom<-switch(input$selected_geom, 
+                    "geom_path"=plottypes[["geom_path"]],
+                    "geom_point"=plottypes[["geom_point"]])
+        
+      
+      #filtering regions and countries
+        
+        if (!is.null(input$selected_area)|!is.null(input$selected_region)){
+          dta<-dta %>% filter(!!sym(names(dta)[2])%in%input$selected_area|
+                               !!sym(names(dta)[3])%in%input$selected_region)
+          }
+       
+        dta1<-dta %>% filter(!is.na(!!y))
+       
+        dta2<-dta %>% filter(!is.na(!!z))
+        
+       
+        
+        #creating the plot
+        gg<-ggplot()+
+        geom(data=dta1,mapping=aes(x=!!x, y=!!y, color=!!col), size=1/4)+ 
+        geom(data=dta2,mapping=aes(x=!!x, y=!!z, color=!!col), size=1/4, linetype = "dashed")
+        
+        s<-ggplotly(gg)
+        s
+      })
+    
+    
+    }
+
+
+
+#Buid App----
+A<-shinyApp(ui=ui, server=server)
+#runApp(A, display.mode = "showcase") this or run in command line: runApp("app-1-lesson4.R", display.mode = "showcase")
+#?runApp
+# A
+#A<-function(dta){A}
+#A(GGDC_large)
